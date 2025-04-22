b5cb08398946c8fc2262f83722d86023033fd92b
Utsav Shah <shahutsav352003@gmail.com>
2025-04-22 15:50:08 +0530

    22-04

--- contact.php
+++ contact.php
@@ -21,7 +21,7 @@
         // Build the email content.
         $email_content = "Name $name\n";
         $email_content .= "Email \n$message\n";
-        $email_content .= "Message \n$message\n";
+        $email_content .= "Message \    n$message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
